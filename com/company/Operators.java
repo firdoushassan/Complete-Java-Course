9235dbc1b7cf8ba1bd0df90cf1122f0cfe453c3c
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-15 01:46:10 +0530

    Add files via upload

---
+++ com/company/Operators.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Operators {
+    public static void main(String[] args){
+        int a = 4;
+//        int b = 6+a;
+        int b = 9;
+        b+=3;           // Add 3 to b
+        System.out.println(b);
+    }
+
+}
