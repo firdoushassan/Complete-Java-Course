f09ba0f38f42454c418b8a2c41ef9e9a32a4b40d
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-15 01:46:59 +0530

    Add files via upload

---
+++ com/company/resulting_data_type.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class resulting_data_type {
+    public static void main(String[] args) {
+//        float a = 6.54f + 6;
+//        System.out.println(a);
+
+        //Increment and Decrement Operators
+
+        int i = 56;
+        System.out.println(i++);    //first print and then increment
+        System.out.println(++i);    //first increment and then print
+    }
+
+}
+
