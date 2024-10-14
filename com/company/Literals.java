03cb43e5ed44d176e38be536efdad65af6782da2
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-15 01:44:08 +0530

    Add files via upload

---
+++ com/company/Literals.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Literals {
+    public static void main(String[] args){
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        long ageDino = 5666666666L;
+        boolean a = true;
+        String str = "Name";
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ageDino);
+        System.out.println(a);
+        System.out.println(str);
+    }
+}
