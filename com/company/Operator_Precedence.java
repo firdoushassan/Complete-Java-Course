9dc6b64caacffdfbc5dbf1f4baf54553be8c6db2
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-15 01:46:32 +0530

    Add files via upload

---
+++ com/company/Operator_Precedence.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Operator_Precedence {
+    public static void main(String[] args){
+        int a = 6*5-34/2;
+        int b = 60/5-34*2;
+        System.out.println(a);
+        System.out.println(b);
+        // Precedence & Associativity
+    }
+}
