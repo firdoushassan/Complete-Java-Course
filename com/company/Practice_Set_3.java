3a6efda67e84f6585d8cd57e9a9a4d6f25507a0d
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-16 11:41:53 +0530

    Add files via upload

---
+++ com/company/Practice_Set_3.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+
+public class Practice_Set_3 {
+    public static void main(String[] args){
+        //Problem
+//        String name = "To Remove Spaces";
+//        name = name.replace(' ', '_');
+//        System.out.println(name);
+
+        //Problem
+//        String letter = "Dear <|name|>, Thanks a lot!";
+//        System.out.println("Enter Name");
+//        Scanner sc = new Scanner(System.in);
+//        String name= sc.nextLine();
+//        letter = letter.replace("<|name|>", name);
+//        System.out.println(letter);
+
+        //Problem
+//        String detect = "To detect  Spaces and   double Spaces";
+//        System.out.println(detect.indexOf("  "));
+//        System.out.println(detect.indexOf("   "));
+
+        //Problem
+        String letter = "Dear Firdous, It is very nice. Thanks a lot!";
+        System.out.println(letter);
+        letter = "Dear Firdous,\n\tIt is very nice. \n\t\t\t\tThanks a lot!";
+        System.out.println(letter);
+    }
+}
