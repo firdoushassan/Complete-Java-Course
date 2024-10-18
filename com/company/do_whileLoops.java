6fdde3e1c0436794c1c6ba6ec90f788687762265
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-18 12:34:33 +0530

    Add files via upload

---
+++ com/company/do_whileLoops.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class do_whileLoops {
+    public static void main(String[] args) {
+        int b = 5;
+        do{
+            System.out.println(b);
+            b--;
+        }while(b<5 && b>0);
+    }
+}
