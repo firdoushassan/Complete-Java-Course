3f2f322b47e24f6bd7c375b631579892f2d5de63
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-20 17:53:43 +0530

    Add files via upload

---
+++ com/company/conditionals.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class conditionals {
+    public static void main(String[] args){
+        int age = 6;
+        if(age>18){
+            System.out.println("Yes,can drive");
+        }else {
+            System.out.println("No cannot drive");
+        }
+
+    }
+}
