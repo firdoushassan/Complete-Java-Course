211fc880fee7fb0057f7e1e5e6d4be550c15ec86
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-30 08:46:32 +0530

    Add files via upload

---
+++ com/company/Inheritance.java
@@ -0,0 +1,38 @@
+package com.company;
+
+class Base{
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void printMe(){
+        System.out.println("This is a constructor");
+    }
+}
+
+class Derived extends Base{
+    int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Derived d = new Derived();
+        d.printMe();                    //
+        d.setX(45);                     //---->Derived from Base class
+        System.out.println(d.getX());   //
+
+    }
+}
