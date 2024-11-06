8dcf4527241d3f2d6df5f37faba5f089ba96aa89
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-11-07 00:37:23 +0530

    Update Packages.java

--- com/company/Packages.java
+++ com/company/Packages.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Packages {
     public static void main(String[] args) {
-
+        //The use of packages is shown in folder packages outside com.company.
     }
 }
