e84627ec8b0d4a91a744ce56ae9a9b2256b20ce0
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-11-17 08:51:43 +0530

    Add files via upload

---
+++ com/company/Collections.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Collections {
+    public static void main(String[] args) {
+//        ArrayList;
+//        Set;
+//        TreeSet;
+    }
+}
