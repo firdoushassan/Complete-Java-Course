a9e2bf426a8d7eba60dccb56374650b1c44a42e7
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-11-24 02:21:19 +0530

    Added Practice Set 15

---
+++ com/company/Practice_Set_15.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+//class depre{
+//    @Deprecated
+//    void meth1(){
+//        System.out.println("Method 1");
+//    }
+//}
+//interface myInt{
+//    void display();
+//}
+
+
+//@SuppressWarnings("depre")
+public class Practice_Set_15 {
+    public static void main(String[] args) {
+        //Problem
+//        depre d = new depre();
+//        d.meth1();
+
+        //Problem
+//        myInt i = new myInt() {
+//            @Override
+//            public void display() {
+//                System.out.println("i am display");
+//            }
+//        };
+//        i.display();
+
+        //Problem
+//        myInt i = () -> System.out.println("i am display");
+//        i.display();
+//
+
+//        File m = new File("Multiplication Table.txt");
+//        try{
+//            m.createNewFile();
+//        }catch(IOException e){
+//            System.out.println("Cannot create File");
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        String table = "";
+//        for(int i =1;i<=10;i++){
+//            table += num+ " x " +i+ " = "+i*num;
+//            table += "\n";
+//        }
+//        sc.close();
+//        try{
+//            FileWriter w = new FileWriter("Multiplication Table.txt");
+//            w.write(table);
+//            w.close();
+//        }catch(IOException e){
+//            System.out.println("Error occurred while writing in the file");
+//        }
+
+    }
+}
